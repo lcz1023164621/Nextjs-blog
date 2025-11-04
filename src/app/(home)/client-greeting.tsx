12696d164f6d0ee39d138ec86a73ec3d4264811c
lcz1023164621 <lcz1023164621@gmail.com>
2025-11-04 18:06:06 +0800

    配置并测试trpc

---
+++ src/app/(home)/client-greeting.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+// <-- hooks can only be used in client components
+import { trpc } from '@/trpc/client';
+export function ClientGreeting() {
+  
+  const greeting = trpc.hello.useQuery({ text: 'lcz' });
+
+  if (!greeting.data) return <div>Loading...</div>;
+  
+  return <div>{greeting.data.greeting}</div>;
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,21 @@
-import { Homeview } from "@/modules/home/view/home-views";
+import { HydrateClient, trpc } from '@/trpc/server';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ClientGreeting } from './client-greeting';
+export default async function Home() {
 
-
-export default function Home() {
+  void trpc.hello.prefetch({ text: 'lcz' });
+  
   return (
-    <div>
-      <Homeview />
-    </div>
+    <HydrateClient>
+      <div>...</div>
+      {/** ... */}
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          
+          <ClientGreeting />
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
   );
 }
