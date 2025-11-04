22031dae589e89db6ac28d6c7d66c834bcd54536
lcz1023164621 <lcz1023164621@gmail.com>
2025-11-04 16:53:34 +0800

    配置clerk

--- src/modules/auth/components/authbutton.tsx
+++ src/modules/auth/components/authbutton.tsx
@@ -1,7 +1,36 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { ClapperboardIcon, UserCircleIcon } from "lucide-react"
+import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs"
+
 export const AuthButton = () => {
     return(
-        <button className="px-4 py-2 rounded-md border border-gray-300">
-            Login
-        </button>
+    <>
+    <SignedIn>
+        <UserButton>
+            <UserButton.MenuItems>
+                <UserButton.Link 
+                    label="Studio"
+                    href="/studio"
+                    labelIcon={<ClapperboardIcon className="size-4"/>}
+                />
+                <UserButton.Action label="manageAccount" />
+            </UserButton.MenuItems>
+        </UserButton>
+    </SignedIn>
+    <SignedOut>
+        <SignInButton mode="modal">
+            <Button variant="outline"
+                className="px-4 py-2 text-sm font-medium text-blue-600 hover:bg-blue-500
+                border-blue-500/20 rounded-full shadow-none [&_svg]:size-5">
+            <UserCircleIcon />
+            Sign in
+            </Button>
+        </SignInButton>
+    </SignedOut>
+
+    </>
+
     )
-};
+}
