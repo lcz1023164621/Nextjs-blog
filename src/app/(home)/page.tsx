31c20d295f3d5ab8e8afed19ec76afe3879bb841
lcz1023164621 <lcz1023164621@gmail.com>
2025-11-23 17:08:45 +0800

    修改部署问题

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,9 @@ import { HydrateClient, trpc } from '@/trpc/server';
 import { Suspense } from 'react';
 import { Homeview } from '@/modules/home/view/home-views';
 
+// 强制动态渲染，禁用静态生成
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
 
   void trpc.hello.prefetch({ text: 'lcz' });
